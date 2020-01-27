86c4a331ce097171d437f1bbd683badf3cd43d73
Jocelyn Beedie <jellonator00@gmail.com>
2020-01-26 19:52:56 -0500

    Added basic game loop

--- character.py
+++ character.py
@@ -1,41 +1,43 @@
 MAX_STAT_VALUE = 10
 
 class CharacterStat:
-    def __init__(self, maxvalue: int):
+    def __init__(self, maxvalue):
         if maxvalue < 1:
             maxvalue = 1
         if maxvalue > MAX_STAT_VALUE:
             maxvalue = MAX_STAT_VALUE
         self.maxvalue = maxvalue
         self.value = maxvalue
-    def reset(self, maxvalue: int):
+    def reset(self, maxvalue):
         if maxvalue < 1:
             maxvalue = 1
         if maxvalue > MAX_STAT_VALUE:
             maxvalue = MAX_STAT_VALUE
         self.maxvalue = maxvalue
         self.value = maxvalue
-    def add(self, amount: int):
-        self.value = self.value + amount
+    def setvalue(self, newvalue):
+        self.value = newvalue
         if self.value > self.maxvalue:
             self.value = self.maxvalue
         if self.value < 0:
             self.value = 0
-    def subtract(self, amount: int):
-        self.add(-amount)
-    def is_empty(self) -> bool:
+    def add(self, amount):
+        self.setvalue(self.value + amount)
+    def subtract(self, amount):
+        self.setvalue(self.value - amount)
+    def is_empty(self):
         return self.value == 0
-    def upgrade(self, amount: int):
+    def upgrade(self, amount):
         if self.maxvalue + amount < 1:
             amount = 1 - self.maxvalue
         if self.maxvalue + amount > MAX_STAT_VALUE:
             amount = MAX_STAT_VALUE - amount
         self.maxvalue += amount
         self.value += amount
-    def get_ratio(self) -> float:
+    def get_ratio(self):
         return self.value / self.maxvalue
-    def format_string(self) -> str:
-        return "|" + "+" * self.value + "-" * (self.maxvalue - self.value) + "|" + " " * (MAX_STAT_VALUE - self.maxvalue)
+    def format_string(self):
+        return "|" + "+" * self.value + "." * (self.maxvalue - self.value) + "|" + " " * (MAX_STAT_VALUE - self.maxvalue)
 
 class Character:
     def __init__(self):
@@ -43,8 +45,47 @@ class Character:
         self.dexterity = CharacterStat(3)
         self.wisdom = CharacterStat(3)
         self.soul = CharacterStat(3)
-    def format_string(self) -> str:
+    def format_string(self):
         return "STR " + self.strength.format_string() + " " \
             "DEX " + self.dexterity.format_string() + " " \
             "WIS " + self.wisdom.format_string() + " " \
             "SOUL " + self.soul.format_string() + " "
+
+def generate_character(classdefs):
+    player = Character()
+    classlist = [name for name in classdefs]
+    classlist.sort()
+    for i, classname in enumerate(classlist):
+        print("{}. {}:\t{}".format(i + 1, classname, classdefs[classname]["description"]))
+    index = -1
+    numclasses = len(classlist)
+    while not index in range(numclasses):
+        try:
+            index = int(input("What is your class? [1-{}]: ".format(numclasses))) - 1
+            if not index in range(numclasses):
+                print("Input is not a valid index.")
+        except TypeError:
+            print("Input is not a valid integer.")
+    chosen_class_name = classlist[index]
+    chosen_class_data = classdefs[chosen_class_name]
+    if "stat_max" in chosen_class_data:
+        class_stats = chosen_class_data["stat_max"]
+        if "STR" in class_stats:
+            player.strength.reset(class_stats["STR"])
+        if "DEX" in class_stats:
+            player.dexterity.reset(class_stats["DEX"])
+        if "WIS" in class_stats:
+            player.wisdom.reset(class_stats["WIS"])
+        if "SOUL" in class_stats:
+            player.soul.reset(class_stats["SOUL"])
+    if "stat_value" in chosen_class_data:
+        class_values = chosen_class_data["stat_value"]
+        if "STR" in class_values:
+            player.strength.setvalue(class_values["STR"])
+        if "DEX" in class_values:
+            player.dexterity.setvalue(class_values["DEX"])
+        if "WIS" in class_values:
+            player.wisdom.setvalue(class_values["WIS"])
+        if "SOUL" in class_values:
+            player.soul.setvalue(class_values["SOUL"])
+    return player
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-import sys, os, json
+import sys, os
 # Check to make sure we are running the correct version of Python
 assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 
 import character
+import json
 
 # The game and item description files (in the same folder as this script)
 FILE_LEVEL = 'zork.json'
@@ -21,42 +22,91 @@ def load_json(name: str):
         print("There was a problem reading either the game or item file.")
         os._exit(1)
 
+class GameData:
+    def __init__(self, leveldata, itemdata, player, startroom):
+        self.level = leveldata
+        self.items = itemdata
+        self.player = player
+        self.room = startroom
+        self.finished = False
+        self.actions = {
+            "help": GameAction(action_help, "You're already using this, dummy!"),
+            "stat": GameAction(action_stat, "Gives information on your current stats."),
+            "quit": GameAction(action_quit, "Quit the game.")
+        }
+
+class GameAction:
+    def __init__(self, func, helptext):
+        self.func = func
+        self.help = helptext
+
+def action_help(gamedata, args):
+    if len(args) == 0:
+        print("Available actions:", ", ".join(gamedata.actions.keys()))
+        print("Type 'help action' for information about the given action.")
+    elif len(args) == 1:
+        name = args[0]
+        if name in gamedata.actions:
+            print(gamedata.actions[name].help)
+    else:
+        print("Too many arguments to 'help'.")
+
+def action_stat(gamedata, args):
+    if len(args) == 0:
+        print(gamedata.player.format_string())
+        print("Type 'stat statname' for more information about the given stat.")
+    elif len(args) == 1:
+        name = args[0]
+        if name in ["str", "strength"]:
+            print("Strength, your physical power and health.")
+            print("If you run out, you die from your injuries.")
+            print("STR:", gamedata.player.strength.format_string())
+        elif name in ["dex", "dexterity"]:
+            print("Dexterity, your agility and stealthiness.")
+            print("If you run out, you cease to move again.")
+            print("DEX:", gamedata.player.dexterity.format_string())
+        elif name in ["wis", "wisdom"]:
+            print("Wisdom, your mental acuity and intelligence.")
+            print("If you run out, you lose the will to keep going.")
+            print("WIS:", gamedata.player.wisdom.format_string())
+        elif name in ["soul"]:
+            print("Soul, your mortal connection.")
+            print("If you run out, you succumb to the darkness and your soul is lost forever.")
+            print("SOUL:", gamedata.player.soul.format_string())
+        else:
+            print("Unknown stat name.")
+    else:
+        print("Too many arguments to 'stat'.")
+
+def action_quit(gamedata, args):
+    if len(args) == 0:
+        gamedata.finished = True
+    else:
+        print("Too many arguments to 'help'.")
+
+def game_loop(gamedata):
+    print("Type 'help' for information.")
+    while not gamedata.finished:
+        userinput = input("> ").lower().strip()
+        userargs = userinput.split()
+        if len(userargs) > 0:
+            action = userargs[0]
+            args = userargs[1:]
+            if action in gamedata.actions:
+                gamedata.actions[action].func(gamedata, args)
+
 # The main function for the game
 def main():
-    current = 'WHOUS'  # The starting location
-    end_game = ['END']  # Any of the end-game locations
+    current = 'WHOUS' # The starting location
 
     level = load_json(FILE_LEVEL)
     items = load_json(FILE_ITEMS)
     classdefs = load_json(FILE_CLASSES)
     
-    player = character.Character()
-    classlist = [name for name in classdefs]
-    classlist.sort()
-    for i, classname in enumerate(classlist):
-        print("{}. {}".format(i + 1, classname))
-    index = -1
-    numclasses = len(classlist)
-    while not index in range(numclasses):
-        try:
-            index = int(input("What is your class? [1-{}]: ".format(numclasses))) - 1
-            if not index in range(numclasses):
-                print("Input is not a valid index.")
-        except TypeError:
-            print("Input is not a valid integer.")
-    chosen_class_name = classlist[index]
-    chosen_class_data = classdefs[chosen_class_name]
-    if "stats" in chosen_class_data:
-        class_stats = chosen_class_data["stats"]
-        if "STR" in class_stats:
-            player.strength.reset(class_stats["STR"])
-        if "DEX" in class_stats:
-            player.dexterity.reset(class_stats["DEX"])
-        if "WIS" in class_stats:
-            player.wisdom.reset(class_stats["WIS"])
-        if "SOUL" in class_stats:
-            player.soul.reset(class_stats["SOUL"])
+    player = character.generate_character(classdefs)
     print(player.format_string())
+    gamedata = GameData(level, items, player, current)
+    game_loop(gamedata)
 
 # run the main function
 if __name__ == '__main__':
