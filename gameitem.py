cb69b0c2f8cd09ad0165bef17cc1448256a3b76c
Jocelyn Beedie <jellonator00@gmail.com>
2020-01-28 16:45:14 -0500

    Made gameaction a little more generalized

--- gameitem.py
+++ gameitem.py
@@ -3,15 +3,67 @@ import gameutil
 def choose_enemy(gamedata):
     return gameutil.choose_from_list(gamedata.encounter, True, "Choose an enemy to attack")
 
-class GameActionAttack:
+ATTACK_MISS = 0
+ATTACK_HIT = 1
+ATTACK_CANCEL = 2
+
+def try_attack_enemy(gamedata, target, stat, stat_negate, attack_bonus):
+    player_stat = gamedata.player.get_stat(stat, False)
+    dice_stat = None
+    player_stat_value = 0
+    if player_stat != None:
+        if stat_negate:
+            player_stat_value = player_stat.maxvalue - player_stat.value + 1
+        else:
+            player_stat_value = player_stat.value
+        dice_stat = gameutil.roll_dice(player_stat_value, 6)
+    else:
+        dice_stat = []
+    if attack_bonus == 0:
+        input("Rolling {}d6 to attack...".format(player_stat_value))
+    else:
+        input("Rolling {}d6 + {}d6...".format(player_stat_value, attack_bonus))
+    dice_bonus = gameutil.roll_dice(attack_bonus, 6)
+    fmt_stat = ' '.join((str(x) for x in dice_stat))
+    roll_total = sum(dice_stat) + sum(dice_bonus)
+    if attack_bonus == 0:
+        print("You rolled: [{} {}] = {}".format(stat.upper(), fmt_stat, roll_total))
+    else:
+        fmt_bonus = ' '.join((str(x) for x in dice_bonus))
+        print("You rolled: [{} {}] [+ {}] = {}".format(stat.upper(), fmt_stat, fmt_bonus, roll_total))
+    input("The {} is rolling {}d6 for defense...".format(target.name, target.defense))
+    target_dice = target.get_defense_roll()
+    target_roll_total = sum(target_dice)
+    fmt_target = ' '.join((str(x) for x in target_dice))
+    print("The {} rolled [{}] = {}".format(target.name, fmt_target, target_roll_total))
+    if roll_total >= target_roll_total:
+        return ATTACK_HIT
+    else:
+        return ATTACK_MISS
+
+class GameAction:
     def __init__(self, attackname, parentitem, attackdef):
         self.name = attackdef.get("name", attackname)
+        self.parentitem = parentitem
+    def format_info(self):
+        return ""
+    def __str__(self):
+        info = self.format_info().strip()
+        if info != "":
+            info = "[{}]".format(info)
+        if self.parentitem != None:
+            return "{} ({}) {}".format(self.name, self.parentitem.name, info)
+        else:
+            return "{} {}".format(self.name, info)
+
+class GameActionAttack(GameAction):
+    def __init__(self, attackname, parentitem, attackdef):
+        super().__init__(attackname, parentitem, attackdef)
         self.target = attackdef.get("target", "single")
         self.stat = attackdef.get("stat", "none").lower()
         self.stat_negate = attackdef.get("stat-negate", False)
         self.bonus = attackdef.get("bonus", 0)
         self.damage = attackdef.get("damage", 1)
-        self.parentitem = parentitem
     def format_info(self):
         c = ""
         if self.stat_negate:
@@ -30,45 +82,15 @@ class GameActionAttack:
                 return False
             shared["target"] = target
         print("Attacking {}".format(target.name))
-        player_stat = gamedata.player.get_stat(self.stat, False)
-        dice_stat = None
-        player_stat_value = 0
-        if player_stat != None:
-            if self.stat_negate:
-                player_stat_value = player_stat.maxvalue - player_stat.value + 1
-            else:
-                player_stat_value = player_stat.value
-            dice_stat = gameutil.roll_dice(player_stat_value, 6)
-        else:
-            dice_stat = []
-        if self.bonus == 0:
-            input("Rolling {}d6 to attack...".format(player_stat_value))
-        else:
-            input("Rolling {}d6 + {}d6...".format(player_stat_value, self.bonus))
-        dice_bonus = gameutil.roll_dice(self.bonus, 6)
-        fmt_stat = ' '.join((str(x) for x in dice_stat))
-        roll_total = sum(dice_stat) + sum(dice_bonus)
-        if self.bonus == 0:
-            print("You rolled: [{} {}] = {}".format(self.stat.upper(), fmt_stat, roll_total))
-        else:
-            fmt_bonus = ' '.join((str(x) for x in dice_bonus))
-            print("You rolled: [{} {}] [+ {}] = {}".format(self.stat.upper(), fmt_stat, fmt_bonus, roll_total))
-        input("The {} is rolling {}d6 for defense...".format(target.name, target.defense))
-        target_dice = target.get_defense_roll()
-        target_roll_total = sum(target_dice)
-        fmt_target = ' '.join((str(x) for x in target_dice))
-        print("The {} rolled [{}] = {}".format(target.name, fmt_target, target_roll_total))
-        if roll_total >= target_roll_total:
+        status = try_attack_enemy(gamedata, target, self.stat, self.stat_negate, self.bonus)
+        if status == ATTACK_HIT:
             print("You hit the {} for {} damage!".format(target.name, self.damage))
             target.health.subtract(self.damage)
-        else:
+        elif status == ATTACK_MISS:
             print("You missed the {}.".format(target.name))
-        return True
-    def __str__(self):
-        if self.parentitem != None:
-            return "{} ({}) [{}]".format(self.name, self.parentitem.name, self.format_info())
         else:
-            return "{} [{}]".format(self.name, self.format_info())
+            return False
+        return True
 
 class GameItem:
     def __init__(self, itemname, itemdef):
